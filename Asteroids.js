5d2848e20c5821e3616261a65952aa83de05ea4c
Jun3-dev <jun.lee73@outlook.com>
2022-10-17 15:19:50 +1100

    Player spaceship can spin left and right from player input

--- Asteroids.js
+++ Asteroids.js
@@ -1,16 +1,52 @@
 const canvas = document.getElementById("canvas");
 const FPS = 60;
 const shipSize = 30;
+const turnRate = 360; //degrees per second
 let ctx = canvas.getContext("2d");
 const ship = {
     x: canvas.width / 2,
     y: canvas.height / 2,
     r: shipSize /2,
     angle: 90 / 180 * Math.PI, //convert to radians
+    rot: 0
 }
+
+//Event Handlers
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
 
 //Game Loop
 setInterval(update, 1000/FPS);
+
+function keyDown(/** @type {KeyboardEvent} */ ev) {
+    switch(ev.keyCode) {
+        case 37: //left arrow
+            ship.rot = turnRate / 180 * Math.PI / FPS;
+            break;
+        case 38: //up arrow
+            break;
+        case 39: //right arrow
+            ship.rot = -turnRate / 180 * Math.PI / FPS;
+            break;
+        case 40: //down arrow
+            break;
+    }
+}
+
+function keyUp() {
+    switch(ev.keyCode) {
+        case 37: //left arrow
+            ship.rot = 0;
+            break;
+        case 38: //up arrow
+            break;
+        case 39: //right arrow
+            ship.rot = 0;
+            break;
+        case 40: //down arrow
+            break;
+    }
+}
 
 function update() {
     //Draw Background
@@ -21,21 +57,25 @@ function update() {
     ctx.lineWidth = shipSize / 20;
     ctx.beginPath();
     ctx.moveTo( //Nose of the ship
-        ship.x + ship.r * Math.cos(ship.angle),
-        ship.y - ship.r * Math.sin(ship.angle)
+        ship.x + 4/3 * ship.r * Math.cos(ship.angle),
+        ship.y - 4/3 * ship.r * Math.sin(ship.angle)
     );
     ctx.lineTo( //Rear leftx
-        ship.x - ship.r * (Math.cos(ship.angle) + Math.sin(ship.angle)),
-        ship.y + ship.r * (Math.sin(ship.angle) - Math.cos(ship.angle))
+        ship.x - ship.r * (2/3 * Math.cos(ship.angle) + Math.sin(ship.angle)),
+        ship.y + ship.r * (2/3 * Math.sin(ship.angle) - Math.cos(ship.angle))
     );
     ctx.lineTo( //Rear right
-        ship.x - ship.r * (Math.cos(ship.angle) - Math.sin(ship.angle)),
-        ship.y + ship.r * (Math.sin(ship.angle) + Math.cos(ship.angle))
+        ship.x - ship.r * (2/3 * Math.cos(ship.angle) - Math.sin(ship.angle)),
+        ship.y + ship.r * (2/3 * Math.sin(ship.angle) + Math.cos(ship.angle))
     );
-    ctx.closePath()
+    ctx.closePath();
     ctx.stroke();
 
     //Rotate the ship
-
+    ship.angle += ship.rot;
     //Move the ship
+
+    //Center dot
+    ctx.fillStyle = "red";
+    ctx.fillRect(ship.x-1, ship.y-1, 2, 2)
 }
